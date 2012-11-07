104a4154c49cb77f82182bf05f030ad70b7303e2
Renat Ayzyatullen <brand20@yandex.ru>
2012-11-07 19:12:23 +0300

    fixrd

--- Interfaces/Interfaces/testingwidgetfacade.cpp
+++ Interfaces/Interfaces/testingwidgetfacade.cpp
@@ -1,12 +1,9 @@
 #include "testingwidgetfacade.hpp"
 
-<<<<<<< HEAD
-TestingWidgetFacade::TestingWidgetFacade(){
 
-=======
+
 TestingWidgetFacade::TestingWidgetFacade() {
-    // ������� ��������� �� �����������, �������� �������, ��������� �� ����������, �������� �����
->>>>>>> 13dcb1a299fe3bfde64ff1891418f66d7c0e17d9
+
     QObject::connect(twptr, SIGNAL(jumpToTaskRequest(QString)), this, SLOT(setValue(displayTaskById(QString))), Qt::AutoConnection);
     QObject::connect(twptr, SIGNAL(finishTestRequest()), this, SLOT(setValue(saveResults())), Qt::AutoConnection);
     QObject::connect(twptr, SIGNAL(startTestRequest()), this, SLOT(setValue(beginTesting())), Qt::AutoConnection);
@@ -46,7 +43,7 @@ void TestingWidgetFacade::displayTaskById(QString id) {
 }
 
 void TestingWidgetFacade::saveResults() {
-<<<<<<< HEAD
+
     ttwptr->toFinishCount();
     tlptr->saveResult();
     QStringList results = tlptr->getResults();
@@ -57,16 +54,5 @@ void TestingWidgetFacade::beginTesting() {
     ttwptr->dumpingOfAnswers();
     QString taskText = tlptr->getTaskTextById("A1");
     ttwptr->showTaskText(taskText);
-=======
-    ttwptr->toFinishCount();//��������� ������
-    tlptr->saveResult();//��������� ���������
-    QStringList results = tlptr->getResults();
-    ttwptr->showResult(results);//�������� ��������� � ������� �� �������
-}
-void TestingWidgetFacade::beginTesting() {
-    ttwptr->toReckon();//������ ������
-    ttwptr->dumpingOfAnswers();//�������� ������
-    QString taskText = tlptr->getTaskTextById("A1");//��������� ������ ������� � �1
-    ttwptr->showTaskText(taskText);//�������� �������
->>>>>>> 13dcb1a299fe3bfde64ff1891418f66d7c0e17d9
+
 }
